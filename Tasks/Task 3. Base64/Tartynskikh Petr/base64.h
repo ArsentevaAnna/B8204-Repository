b9b8c6e60b4d4ccf6e0f94f93e0d10bcdbfb262b
HaidenTenno <haiden_tenno@mail.ru>
2017-05-14 20:34:17 +1000

    Че-то

---
+++ Tasks/Task 3. Base64/Tartynskikh Petr/base64.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+using namespace std;
+
+string encode(const char*, int len);
+string decode(string s);
---
+++ Tasks/Task 3. Base64/Tartynskikh Petr/task3.cpp
@@ -0,0 +1,19 @@
+﻿#include "stdafx.h"
+#include <iostream>
+#include "base64.h"
+
+using namespace std;
+
+int main() 
+{
+	string s = "Shaz bi v dotku";
+
+	string encoded = encode(s.c_str(), s.length());
+	string decoded = decode(encoded);
+	cout << "Encoded: " << encoded << endl;
+	cout << "Decoded: " << decoded << endl;
+
+	getchar();
+
+	return 0;
+}
